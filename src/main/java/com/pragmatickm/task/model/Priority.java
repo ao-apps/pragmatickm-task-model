a8722633c2ed013db65be25e5d6a1d63d872e725
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:11 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/pragmatickm/task/model/Priority.java
+++ src/main/java/com/pragmatickm/task/model/Priority.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-task-model - Tasks nested within SemanticCMS pages and elements.
- * Copyright (C) 2013, 2014, 2015, 2016, 2020  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2020, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-task-model.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.task.model;
 
 /**
